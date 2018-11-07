1a7c9a3f79c0f3bf3136fc0d2758765683c28c55
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-11-07 15:45:55 -0600

    data object set (fantasy subgenres)

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,52 +4,69 @@
 
 const objects = [
  {
-    name: "Test",
-    photo: "Test-pic",
+    name: "Alternate History",
+    photo: "https://images-eds-ssl.xboxlive.com/image?url=8Oaj9Ryq1G1_p3lLnXlsaZgGzAie6Mnu24_PawYuDYIoH77pJ.X5Z.MqQPibUVTcAbauIasMcFdLhkDqgr3fitKTpAuTNP5lA8YzolD8iMvNbgdp9WVSvC.XHLzZleO9AbXPjY_7SnYDGY06M4kHIapjAf_wlJN5zhkAengrjVKQ_31k9g24OEyOQ9vuyBvFTUoHQRrS1gEz047nCcIMYrZ6j6el1eY.a7x8bWJIbK0-&h=1080&w=1920&format=jpg",
     score: [
         5,
+        1,
+        2,
+        4,
+        1,
+        4,
+        3,
+        2,
         5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5
+        2
     ]
 },
 {
-    name: "Test-2",
-    photo: "Test-pic-2",
+    name: "High Fantasy",
+    photo: "https://vignette.wikia.nocookie.net/lotr/images/3/33/Legolas_-_in_Two_Towers.PNG/revision/latest?cb=20120916035151",
     score: [
         1,
+        5,
+        5,
         1,
         1,
         1,
         1,
         1,
-        1,
-        1,
-        1,
-        2
+        2,
+        5
     ]
 
 },
 {
-    name: "Test-3",
-    photo: "Test-pic-3",
+    name: "Grim/Low Fantasy",
+    photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/episodes/1/game-of-thrones-1-1920x1080.jpg",
     score: [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
+        2,
+        3,
+        2,
+        5,
+        5,
+        3,
+        3,
+        3,
         1,
         1
+    ]
+
+},
+{
+    name: "Sword and Sorcery",
+    photo: "https://i.pinimg.com/originals/c4/a1/4e/c4a14e105b92ed2c61e42534f14e15fc.jpg",
+    score: [
+        1,
+        2,
+        1,
+        4,
+        3,
+        4,
+        5,
+        2,
+        3,
+        2
     ]
 
 }
